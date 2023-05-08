a9e18a4120c45305c62c9f9eacc2dcef2835dbca
Victor Lucio <>
2023-05-08 10:49:17 -0300

    Adicionando o código

--- main.py
+++ main.py
@@ -3,6 +3,27 @@
 
 def largest_sum(numbers: list[int]) -> tuple[int, int]:
     """Encontra e retorna os dois números que somados dão o maior valor."""
-    primeiro = 0  # o primeiro número da soma
-    segundo = 0  # o segundo número da soma
+    if len(numbers) < 2:
+        return None
+    
+    primeiro = numbers[0]  # o primeiro número da soma
+    segundo = numbers[1]  # o segundo número da soma
+    soma = numbers[0] + numbers[1]
+
+    for i in range(len(numbers)-1):
+        for j in range(i, len(numbers)):
+            if i != j:
+                primeirotest = numbers[i]
+                segundotest = numbers[j]
+                somatest = primeirotest + segundotest
+                if (somatest > soma):
+                    primeiro = primeirotest
+                    segundo = segundotest
+                    soma = somatest
+    
+    if (primeiro > segundo):
+        aux = primeiro
+        primeiro = segundo
+        segundo = aux
+    
     return primeiro, segundo
